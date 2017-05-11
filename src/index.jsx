45a055dc09d4c199b1ea4f31b1b6f417d587737a
Jack Essom <jackessom@hotmail.co.uk>
2017-05-11 01:21:06 -0700

    offline init amend

--- src/index.jsx
+++ src/index.jsx
@@ -5,18 +5,18 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import localForage from 'localforage';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import 'normalize.css';
 import './styles/fonts.css';
 import reducers from './reducers';
 import App from './containers/app/App';
 
+OfflinePluginRuntime.install();
+
 let middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
   const { logger } = require('redux-logger'); // eslint-disable-line global-require
   middleware = [...middleware, logger];
-} else {
-  const OfflinePluginRuntime = require('offline-plugin/runtime'); // eslint-disable-line global-require
-  OfflinePluginRuntime.install();
 }
 
 const store = createStore(
